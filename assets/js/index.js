fcaecc321e6eab36691d4ec1a65f25fb066a83b9
unknown <eveaka24@gmail.com>
2022-03-21 12:39:19 +0300

    adding new files,including JS

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+let firstName = "Eve";
+let lastName = "Akansasira";
+let age = 28;
+const dateOfBirth = "23rd/oct/1993";
+let fullName = firstName + " "+ lastName;
+console.log(fullName);
+
+console.log('hallo, your full name is:' +  fullName +' and you are:'+age +" " + "years old" +" " + "because you were born on" +" " + dateOfBirth);
+
+console.log(firstName);
